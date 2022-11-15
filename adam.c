2c743e3bbb1144777e83d7cfff9af5ca90f8b2b6
PeaceTem <olumidejonathan10@gmail.com>
2022-11-15 13:29:23 +0100

    The assignment

--- adam.c
+++ adam.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include "stdlib.h"
 char adam_steps[100];
 FILE *ptr;
 
 
-void acceptInput();
+// void acceptInput();
 void checkInput();
-int numberOfStep(char *steps);
+void numberOfStep(const char *steps);
 
 int main(int argc, char *argv[])
 {
@@ -18,9 +19,11 @@ int main(int argc, char *argv[])
         printf("Can't open file!");
         return 0;
     }
-    acceptInput();
+    // acceptInput();
+    checkInput();
 
-    numberOfStep(adam_steps);
+    // numberOfStep(adam_steps);
+    fclose(ptr);
     return 0;
 }
 
@@ -35,26 +38,38 @@ void acceptInput()
 
 void checkInput()
 {
-    while(fgets(adam_steps, 100, ptr) != NULL)
-    {
-        printf("%s", adam_steps);
             
+    int number_of_steps = atoi(adam_steps);
+    
+    if(number_of_steps > 0)           
+    {
+        printf("%d\n", number_of_steps);
+    }
+
+    while(fscanf(ptr, "%s", adam_steps) == 1)
+    {
+        // printf("%s\n", adam_steps);
+
         int steps_length = strlen(adam_steps);
-        int counter;
+        // printf("%d\n", steps_length);
+
+        int counter = 0;
         for(int i=0; i<steps_length; i++)
         {
             if(!((adam_steps[i] == 'U') || (adam_steps[i] == 'D')))
             {
                 printf("%c cannot be accepted.\nInput U and D only. Thanks! \n", adam_steps[i]);
-                acceptInput();
+                continue;
+                // acceptInput();
             }
         }
+        numberOfStep(adam_steps);
     }
 
 }
 
 
-int numberOfStep(char *steps)
+void numberOfStep(const char *steps)
 {
     int steps_length = strlen(steps);
     int counter = 0;
@@ -63,13 +78,12 @@ int numberOfStep(char *steps)
         if(steps[i] == 'U')
         {
             counter++;
-            printf("%c\n", steps[i]);
-            printf("%d\n", counter);
+            // printf("%c\n", steps[i]);
+            // printf("%d\n", counter);
         } else if(steps[i] == 'D'){
             break;
         }
     }
     printf("%d\n", counter);
 
-    return 1;
 }
